9f402f406588260285a2e535f491d63e4bf14570
Laykajenta <marianne.odegaard@outlook.com>
2025-06-08 14:30:55 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        Ha en strålende dag!
+
+      </div>
+    </>
+  )
+}
+
+export default App
