8949dda192e49211aaadf2c384feed02327188fb
Laykajenta <marianne.odegaard@outlook.com>
2025-06-08 17:10:51 +0200

    Deleted app.css and added a sass-style file

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,45 @@
-import { useState } from 'react'
-
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const categories = [
+    {
+      id: 1,
+      title: "Hatter",
+    },
+    {
+      id: 2,
+      title: "Sko",
+    },
+    {
+      id: 3,
+      title: "Jakker",
+    },
+    {
+      id: 4,
+      title: "Damer",
+    },
+    {
+      id: 5,
+      title: "Menn",
+    },
+  ];
 
   return (
     <>
-      <div>
-        Ha en strålende dag!
-
+      <div className="categories-container">
+        {categories.map(({ title }) => (
+          <div className="category-container">
+            <div className="category-body-container">
+              <div className="background-image"></div>
+              <h2>{title}</h2>
+              <p>Kjøp nå</p>
+            </div>
+          </div>
+        ))}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
