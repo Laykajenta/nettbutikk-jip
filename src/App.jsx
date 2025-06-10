d3ca9f835d53da73c62e979866c40b45cb6733b8
Laykajenta <marianne.odegaard@outlook.com>
2025-06-10 23:22:45 +0200

    style updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,43 @@
 import { useState } from "react";
+import './categories.styles.scss'
 
 function App() {
   const [count, setCount] = useState(0);
   const categories = [
-    {
+     {
       id: 1,
-      title: "Hatter",
+      title: 'Hatter',
+      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
     },
     {
       id: 2,
-      title: "Sko",
+      title: 'Jakker',
+      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
     },
     {
       id: 3,
-      title: "Jakker",
+      title: 'Sko',
+      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
     },
     {
       id: 4,
-      title: "Damer",
+      title: 'Damer',
+      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
     },
     {
       id: 5,
-      title: "Menn",
+      title: 'Menn',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
     },
   ];
 
   return (
     <>
       <div className="categories-container">
-        {categories.map(({ title }) => (
-          <div className="category-container">
+        {categories.map(({ title, id,imageUrl }) => (
+          <div key={id} className="category-container">
             <div className="category-body-container">
-              <div className="background-image"></div>
+              <div className="background-image" style={{ backgroundImage: `url(${imageUrl})`, }}></div>
               <h2>{title}</h2>
               <p>Kjøp nå</p>
             </div>
