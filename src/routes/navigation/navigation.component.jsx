2e1f7fc8f17a698765e1af1146d04ca14b0b194a
Laykajenta <marianne.odegaard@outlook.com>
2025-08-16 09:52:47 +0200

    added some navigation

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,28 @@
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+import MinLogo from '../../assets/crown.svg?react';
+import './navigation.style.scss';
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to='/'>
+          
+          <MinLogo className='logo' />
+    
+          
+        </Link>
+              <div className="nav-links-container">
+                  <Link className="nav-link" to='/shop'>
+                      BUTIKK                    
+               </Link>   
+              </div>
+              
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
